1045a476c8ce29597ad516662fdd6d055161b390
Yang Hu <whoyoung@gatech.edu>
2019-08-05 17:38:00 -0400

    prevent duplicate title being added

--- notes.js
+++ notes.js
@@ -6,13 +6,20 @@ const getNotes = function() {
 
 const addNotes = function(title, body) {
   const notes = loadNotes();
-  notes.push({
-    title: title,
-    body: body,
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
   })
 
-  saveNotes(notes);
-
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    })
+    saveNotes(notes);
+    console.log('New note added!');
+  } else {
+    console.log('Title already exists!');
+  }
 };
 
 const loadNotes = function () {
